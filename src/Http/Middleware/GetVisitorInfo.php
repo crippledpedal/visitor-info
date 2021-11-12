a25c8545dc86f90e2297b376c5f1e8393c2e6969
Furkan Aksoy <furkan@mezbilisim.com>
2021-11-12 16:24:45 +0300

    verilre kaydetme deneniyor

--- src/Http/Middleware/GetVisitorInfo.php
+++ src/Http/Middleware/GetVisitorInfo.php
@@ -6,22 +6,25 @@ use Carbon\Carbon;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use Mezbilisim\VisitorInfo\Http\Models\Visitor;
 
 class GetVisitorInfo
 {
     public function handle(Request $request, Closure $next)
     {
-        $visitorData = [
-            'ip'      => $request->ipinfo->ip ?? null,
-            'country' => $request->ipinfo->county_name ?? null,
-            'city'    => '',
-            'url'     => '',
-        ];
-
         $requestPath = $request->getPathInfo();
         $explodePath = explode('/', $requestPath);
         array_shift($explodePath);
 
+        $visitorData = [
+            'ip'      => $request->ipinfo->ip ?? null,
+            'country' => $request->ipinfo->county_name ?? null,
+            'city'    => 'deneme',
+            'url'     => $explodePath,
+        ];
+
+        Visitor::create($visitorData);
+        
 
         return $next($request);
     }
--- src/VisitorInfoServiceProvider.php
+++ src/VisitorInfoServiceProvider.php
@@ -19,6 +19,7 @@ class VisitorInfoServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__ . '/database/migrations' => database_path('migrations'),
+            __DIR__ . '/Models' => app_path('Models'),
         ]);
     }
     public function register()
