4db2d5f4628b5e082038f4b14622e9fbeff703d1
Furkan Aksoy <furkan@mezbilisim.com>
2021-11-13 21:16:53 +0300

    css linkleri güncellendi

--- src/views/visitors.blade.php
+++ src/views/visitors.blade.php
@@ -21,28 +21,28 @@
         <link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7CShadows+Into+Light&display=swap" rel="stylesheet" type="text/css">
 
         <!-- Vendor CSS -->
-        <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('vendor/animate/animate.compat.css') }}">
-        <link rel="stylesheet" href="{{ asset('vendor/simple-line-icons/css/simple-line-icons.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('vendor/owl.carousel/assets/owl.carousel.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('vendor/owl.carousel/assets/owl.theme.default.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('vendor/magnific-popup/magnific-popup.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('visitor-info/vendor/bootstrap/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('visitor-info/vendor/fontawesome-free/css/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('visitor-info/vendor/animate/animate.compat.css') }}">
+        <link rel="stylesheet" href="{{ asset('visitor-info/vendor/simple-line-icons/css/simple-line-icons.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('visitor-info/vendor/owl.carousel/assets/owl.carousel.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('visitor-info/vendor/owl.carousel/assets/owl.theme.default.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('visitor-info/vendor/magnific-popup/magnific-popup.min.css') }}">
 
         <!-- Theme CSS -->
-        <link rel="stylesheet" href="{{ asset('css/theme.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/theme-elements.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/theme-blog.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/theme-shop.css') }}">
+        <link rel="stylesheet" href="{{ asset('visitor-info/css/theme.css') }}">
+        <link rel="stylesheet" href="{{ asset('visitor-info/css/theme-elements.css') }}">
+        <link rel="stylesheet" href="{{ asset('visitor-info/css/theme-blog.css') }}">
+        <link rel="stylesheet" href="{{ asset('visitor-info/css/theme-shop.css') }}">
 
         <!-- Skin CSS -->
-        <link rel="stylesheet" href="{{ asset('css/skins/default.css') }}">
+        <link rel="stylesheet" href="{{ asset('visitor-info/css/skins/default.css') }}">
 
         <!-- Theme Custom CSS -->
-        <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+        <link rel="stylesheet" href="{{ asset('visitor-info/css/custom.css') }}">
 
         <!-- Head Libs -->
-        <script src="{{ asset('vendor/modernizr/modernizr.min.js') }}"></script>
+        <script src="{{ asset('visitor-info/vendor/modernizr/modernizr.min.js') }}"></script>
 
     </head>
     <body data-plugin-page-transition>
