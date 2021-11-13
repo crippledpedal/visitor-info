0cff38d6f422fffe77e35029ebbb41391edc6efd
Furkan Aksoy <furkan@mezbilisim.com>
2021-11-13 23:31:58 +0300

    route nameler güncelledni

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'Mezbilisim\VisitorInfo\Http\Controller'], function () {
-    Route::get('visitors', 'VisitorInfoController@index');
-    Route::post('visitors', 'VisitorInfoController@download');
+    Route::get('visitors', 'VisitorInfoController@index')->name('visitors');
+    Route::post('visitors', 'VisitorInfoController@download')->name('visitors');
 });
